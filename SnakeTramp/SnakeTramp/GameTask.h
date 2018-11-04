5c72753a1c5ff42abbce2b555ec428337ded6122
starcatneko <starcatneko@gmail.com>
2018-11-05 02:41:15 +0900

    準備
    全ての準備

--- SnakeTramp/SnakeTramp/GameTask.h
+++ SnakeTramp/SnakeTramp/GameTask.h
@@ -1,8 +1,31 @@
 #pragma once
+#include <memory>
+
+#define lpGameTask GameTask::GetInstance()
+
 class GameTask
 {
 public:
+	static GameTask &GetInstance()
+	{
+		return *s_Instance;
+	};
+
+	void Run();
+
+private:
+	struct GameTaskDeleter
+	{
+		void operator ()(GameTask* GameTask) const
+		{
+			delete GameTask;
+		}
+	};
+
+	static std::unique_ptr<GameTask, GameTaskDeleter> s_Instance;
+
 	GameTask();
 	~GameTask();
+
 };
 
--- SnakeTramp/SnakeTramp/main.cpp
+++ SnakeTramp/SnakeTramp/main.cpp
@@ -1,18 +1,24 @@
 #include "DxLib.h"
+#include "GameTask.h"
+
+#define SCREEN_SIZE_X 800
+#define SCREEN_SIZE_Y 600
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 16);
+	ChangeWindowMode(true);
+	SetDrawScreen(DX_SCREEN_BACK);
+
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+	lpGameTask.Run();
 
 	WaitKey();				// キー入力待ち
-
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
-
 	return 0;				// ソフトの終了 
 }
