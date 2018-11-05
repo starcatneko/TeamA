6234d6e120921a4f0f52f35ada53bef139566ea1
starcatneko <star.cat.neko@gmail.com>
2018-11-05 10:06:10 +0900

    わいわいリゾート

--- SnakeTramp/SnakeTramp/Card.h
+++ SnakeTramp/SnakeTramp/Card.h
@@ -1,8 +1,24 @@
 #pragma once
+
+// カードのスート（柄）用
+enum CARD_SUIT {
+	SUIT_SPADE,
+	SUIT_CRUB,
+	SUIT_DIA,
+	SUIT_HEART,
+	SUIT_NUM
+};
+
 class Card
 {
 public:
 	Card();
 	~Card();
+private:
+	// カードのスート（柄）
+	CARD_SUIT suit;
+	// カードの数値 1~13の範囲内
+	int number;
+	
 };
 
--- SnakeTramp/SnakeTramp/Player.h
+++ SnakeTramp/SnakeTramp/Player.h
@@ -1,8 +1,18 @@
 #pragma once
+
+enum CARD_SUIT;
+
 class Player
 {
 public:
 	Player();
 	~Player();
+
+	// プレイヤーの現在のスート（柄）
+	CARD_SUIT suit;
+	// プレイヤーの現在の数値
+	int number;
+
+
 };
 
