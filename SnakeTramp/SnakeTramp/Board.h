da0cc278a65d0cf99f358abb4444eb81596c2b1e
kimura <kimurano3710@gmail.com>
2018-11-06 10:06:21 +0900

    kosuin

--- SnakeTramp/SnakeTramp/Board.h
+++ SnakeTramp/SnakeTramp/Board.h
@@ -1,8 +1,23 @@
 #pragma once
 
+#define BOARD_DEF_TROUT_X		(7)		// Ï½‚ÌÃŞÌ«ÙÄ‚Ì”
+#define BOARD_DEF_TROUT_Y		(7)		// Ï½‚ÌÃŞÌ«ÙÄ‚Ì”
+
+class VECTOR2;
+
 class Board
 {
 public:
-	Board();
+	Board(bool defFlag, VECTOR2 troutCnt);
+	VECTOR2 BoardTrout();			// ÎŞ°ÄŞ‚ÌÏ½”‚ğ•ÏX‚µ‚½‚¢‚È‚ç‚±‚±‚©‚ç
+private:
 	~Board();
+	bool CheckBoard(VECTOR2 pos);
+	bool SetBoard(VECTOR2 pos);
+
+	bool changeFlag;
+
+	VECTOR2 troutCnt;
+
 };
+
